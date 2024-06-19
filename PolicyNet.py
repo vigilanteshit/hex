e12794185b116e01ece76a24dd362b59078ba179
Wolfgang-Wilke <wolfgang.wilke-public@protonmail.com>
2024-06-19 22:33:48 +0200

    added ResNet

--- PolicyNet.py
+++ PolicyNet.py
@@ -132,3 +132,99 @@ def create_dense_nn_policy_net(board_size):
         model.name = "dense_nn_net_"
         
         return model
+
+
+
+
+
+def identity_block(X, f, filters, stage, block):
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    
+    F1, F2, F3 = filters
+
+    X_shortcut = X
+
+    X = layers.Conv2D(filters=F1, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2a')(X)
+    X = layers.BatchNormalization(axis=3, name=bn_name_base + '2a')(X)
+    X = layers.Activation('relu')(X)
+
+    X = layers.Conv2D(filters=F2, kernel_size=(f, f), strides=(1, 1), padding='same', name=conv_name_base + '2b')(X)
+    X = layers.BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
+    X = layers.Activation('relu')(X)
+
+    X = layers.Conv2D(filters=F3, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2c')(X)
+    X = layers.BatchNormalization(axis=3, name=bn_name_base + '2c')(X)
+
+    X = layers.Add()([X, X_shortcut])
+    X = layers.Activation('relu')(X)
+
+    return X
+
+def convolutional_block(X, f, filters, stage, block, s=2):
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    
+    F1, F2, F3 = filters
+
+    X_shortcut = X
+
+    X = layers.Conv2D(F1, (1, 1), strides=(s, s), name=conv_name_base + '2a')(X)
+    X = layers.BatchNormalization(axis=3, name=bn_name_base + '2a')(X)
+    X = layers.Activation('relu')(X)
+
+    X = layers.Conv2D(F2, (f, f), strides=(1, 1), padding='same', name=conv_name_base + '2b')(X)
+    X = layers.BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
+    X = layers.Activation('relu')(X)
+
+    X = layers.Conv2D(F3, (1, 1), strides=(1, 1), name=conv_name_base + '2c')(X)
+    X = layers.BatchNormalization(axis=3, name=bn_name_base + '2c')(X)
+
+    X_shortcut = layers.Conv2D(F3, (1, 1), strides=(s, s), name=conv_name_base + '1')(X_shortcut)
+    X_shortcut = layers.BatchNormalization(axis=3, name=bn_name_base + '1')(X_shortcut)
+
+    X = layers.Add()([X, X_shortcut])
+    X = layers.Activation('relu')(X)
+
+    return X
+
+def ResNet50(input_shape=(64, 64, 1), classes=64*64):
+    X_input = Input(input_shape)
+
+    # Adjusting the initial convolution layer for smaller input sizes
+    X = layers.Conv2D(64, (3, 3), strides=(1, 1), padding='same', name='conv1')(X_input)
+    X = layers.BatchNormalization(axis=3, name='bn_conv1')(X)
+    X = layers.Activation('relu')(X)
+
+    # Only apply MaxPooling if the input size is large enough
+    if input_shape[0] > 3 and input_shape[1] > 3:
+        X = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), padding='same')(X)
+
+    X = convolutional_block(X, f=3, filters=[64, 64, 256], stage=2, block='a', s=1)
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='b')
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='c')
+
+    X = convolutional_block(X, f=3, filters=[128, 128, 512], stage=3, block='a', s=2)
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='b')
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='c')
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='d')
+
+    X = convolutional_block(X, f=3, filters=[256, 256, 1024], stage=4, block='a', s=2)
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='b')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='c')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='d')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='e')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='f')
+
+    X = convolutional_block(X, f=3, filters=[512, 512, 2048], stage=5, block='a', s=2)
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='b')
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='c')
+
+    X = layers.GlobalAveragePooling2D()(X)
+    X = layers.Dense(classes, activation='softmax', name='fc' + str(classes))(X)
+
+    model = models.Model(inputs=X_input, outputs=X, name='ResNet50')
+
+    return model
+
+
